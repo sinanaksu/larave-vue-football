c87067a7a48afa5d22d560e81ec1f2f2350af958
Sinan Aksu <sinanksu@gmail.com>
2022-01-13 14:48:24 +0300

    chore: add package vue, vuex, bootstrap

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <div class="container">
+            {{ test }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            test: "Sinan",
+        };
+    },
+    components: {},
+    methods: {},
+    computed: {},
+};
+</script>
+
+<style lang="css">
+#app {
+    min-height: 100vh;
+    background-color: rgb(233, 233, 233);
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import Vuex from 'vuex';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+Vue.config.productionTip = false
+Vue.use(Vuex);
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
