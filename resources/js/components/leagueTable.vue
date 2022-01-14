457dcbc34542f66d980e202d6e996c6aef97ac46
Sinan Aksu <sinanksu@gmail.com>
2022-01-14 15:31:59 +0300

    feat: add odds and stadium league table

--- resources/js/components/leagueTable.vue
+++ resources/js/components/leagueTable.vue
@@ -6,20 +6,23 @@
                     <tr>
                         <th width="55"></th>
                         <th>Team Name</th>
-                        <th v-if="detail">Played</th>
+                        <th v-if="!detail">Stadium</th>
+                        <th v-if="detail">M</th>
                         <th v-if="detail">W</th>
                         <th v-if="detail">D</th>
                         <th v-if="detail">L</th>
                         <th v-if="detail">GF</th>
                         <th v-if="detail">GA</th>
                         <th v-if="detail">GD</th>
-                        <th v-if="detail">Points</th>
+                        <th v-if="detail">P</th>
+                        <th class="text-center bg-warning text-dark" v-if="week == 4 || week == 5">ODDS</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="(team, index) of teams" :key="index">
                         <td><img :src="team.logo" width="25" /></td>
                         <td>{{ team.name }}</td>
+                        <td v-if="!detail">{{ team.stadium }}</td>
                         <td v-if="detail">{{ team.played }}</td>
                         <td v-if="detail">{{ team.win }}</td>
                         <td v-if="detail">{{ team.drawn }}</td>
@@ -28,6 +31,7 @@
                         <td v-if="detail">{{ team.ga }}</td>
                         <td v-if="detail">{{ team.gd }}</td>
                         <td v-if="detail">{{ team.points }}</td>
+                        <td class="text-center bg-warning" v-if="week == 4 || week == 5">{{ team.odds }}%</td>
                     </tr>
                 </tbody>
             </table>
@@ -40,7 +44,8 @@ export default {
     name: "leagueTable",
     props: {
         teams: Array,
-        detail: Boolean
+        detail: Boolean,
+        week: Number
     }
 };
 </script>
