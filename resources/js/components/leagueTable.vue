13359c1695dbd8ec351c1505809f8e23c59643c6
Sinan Aksu <sinanksu@gmail.com>
2022-01-14 05:52:51 +0300

    feat: add league-table.vue to vue.js

---
+++ resources/js/components/leagueTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row ps-1 pe-1">
+    <div class="col">
+        <table class="table">
+            <tr>
+                <th></th>
+                <th>Team Name</th>
+                <th v-if="detail">Played</th>
+                <th v-if="detail">W</th>
+                <th v-if="detail">D</th>
+                <th v-if="detail">L</th>
+                <th v-if="detail">GF</th>
+                <th v-if="detail">GA</th>
+                <th v-if="detail">GD</th>
+                <th v-if="detail">Points</th>
+            </tr>
+            <tr  v-for="(team, index) of teams" :key="index">
+                <td><img :src="team.logo" width="40" /></td>
+                <td>{{team.name}}</td>
+                <td v-if="detail">{{team.played}}</td>
+                <td v-if="detail">{{team.win}}</td>
+                <td v-if="detail">{{team.drawn}}</td>
+                <td v-if="detail">{{team.lost}}</td>
+                <td v-if="detail">{{team.gf}}</td>
+                <td v-if="detail">{{team.ga}}</td>
+                <td v-if="detail">{{team.gd}}</td>
+                <td v-if="detail">{{team.points}}</td>
+            </tr>
+        </table>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "leagueTable",
+  props: {
+    teams: Array,
+    detail: Boolean
+  }
+};
+</script>
