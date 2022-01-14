1ff86125c7c388034656fb8d57aed316c610d557
Sinan Aksu <sinanksu@gmail.com>
2022-01-14 15:10:04 +0300

    feat: change index function to match controller

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Match;
+use App\Models\Team;
 use Illuminate\Http\Request;
 
 class MatchController extends Controller
@@ -14,7 +15,41 @@ class MatchController extends Controller
      */
     public function index()
     {
-        //
+        $matches = Match::orderBy('week', 'asc')
+        ->get()
+        ->toArray();
+        foreach ($matches as $key => $value) {
+            $matches[$key]["sss"] = $value["home_id"];
+            $matches[$key]["home_team"] = Team::select('name', 'logo')->where('id', $value["home_id"])
+            ->get()
+            ->toArray();
+            $matches[$key]["away_team"] = Team::select('name', 'logo')->where('id', $value["away_id"])
+            ->get()
+            ->toArray();
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $matches
+        ]);
+    }
+
+    public function generateMatches(Request $request)
+    {
+        $seeder = new \Database\Seeders\MatchSeeder();
+        try {
+
+            $seeder->run();
+            return $this->index();
+
+        } catch (\Throwable $th) {
+
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage()
+            ]);
+
+        }
     }
 
     /**
